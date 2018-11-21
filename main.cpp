66429c6092a6ab633a9d86ba3d013091904df84b
ringoball <jonandovb@hotmail.com>
2018-11-21 19:00:57 -0200

    delete

--- main.cpp
+++ main.cpp
@@ -49,36 +49,6 @@ void changeScale(float i, float j, float k) {
 	scaleZ = k;
 }
 
-void outroobjeto()
-{
-
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glColor3f(R,G,B);
-
-	glPushMatrix();
-
-	        // cria matrizes de transforma��o
-	glRotatef ((GLfloat) rotationX, 1.0, 0.0, 0.0);
-	glRotatef ((GLfloat) rotationY, 0.0, 1.0, 0.0);
-	glRotatef ((GLfloat) rotationZ, 0.0, 0.0, 1.0);
-
-	glTranslatef ((GLfloat) translationX, 0.0, 0.0);
-	glTranslatef (0.0, (GLfloat) translationY, 0.0);
-
-	glScalef(scaleX, scaleY, scaleZ);
-
-	// desenha o objeto
-	if(drawFormat)
-		drawWire(shape);
-	else
-		draw(shape);
-
-	glPopMatrix();
-
-	glutSwapBuffers();
-
-}
-
 // fun��o callback chamada para fazer o desenho
 void display()
 {
